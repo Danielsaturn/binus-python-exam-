c4c87cdb3f921af5b3c401cd9a29bcea4bbcd41a
Danielsaturn <danielbussiness2000@gmail.com>
2025-01-11 11:20:58 +0700

    Create Exam1.py

---
+++ Exam1.py
@@ -0,0 +1,7 @@
+varb = float(input("Tolong masukan variable b: "))
+vara = float(input("Tolong masukan variable a: "))
+vary = float(input("Tolong masukan variable y: "))
+
+result = (varb - 10) / (2 * (1 - (vara / (2 * vary**2))))
+
+print("Hasil dari variable yang dimasukan: ", result)
